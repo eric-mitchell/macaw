3dfd4f9f8469003ea9c9676da4f53f34d19318ff
Eric Mitchell <em7@stanford.edu>
2021-12-06 22:56:47 -0800

    Corrected bad copy of scripts/macaw_vel.sh.

--- scripts/macaw_vel.sh
+++ scripts/macaw_vel.sh
@@ -3,9 +3,9 @@
 source env/bin/activate
 which python
 
-NAME="macaw_vel_hotstart"
+NAME="macaw_vel"
 LOG_DIR="log"
 TASK_CONFIG="config/cheetah_vel/40tasks_offline.json"
-MACAW_PARAMS="config/alg/standard_loadvel.json"
+MACAW_PARAMS="config/alg/standard.json"
 
 ./scripts/runner.sh $NAME $LOG_DIR $TASK_CONFIG $MACAW_PARAMS

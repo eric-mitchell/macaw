1d84c4a496aa9987e12f1e5ecc483c1f45877b03
Daniel Havir <havir.daniel@gmail.com>
2022-11-30 00:04:33 -0800

    update scripts

--- scripts/macaw_dir.sh
+++ scripts/macaw_dir.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-source env/bin/activate
+#source env/bin/activate
 which python
 
 NAME="macaw_dir"
 LOG_DIR="log"
 TASK_CONFIG="config/cheetah_dir/2tasks_offline.json"
 MACAW_PARAMS="config/alg/standard.json"
+if [ -z $1 ]
+then
+    OVERRIDE_PARAMS="config/alg/overrides/no_override.json"
+else
+    OVERRIDE_PARAMS=$1
+fi
 
-./scripts/runner.sh $NAME $LOG_DIR $TASK_CONFIG $MACAW_PARAMS
+./scripts/runner.sh $NAME $LOG_DIR $TASK_CONFIG $MACAW_PARAMS $OVERRIDE_PARAMS
